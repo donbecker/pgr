2042ce425727364e597be35bfa48edf42817e882
donbecker <donbecker@donbeckeronline.com>
2022-09-07 17:17:10 -0400

    refactor API call to a separate function

--- pgr.py
+++ pgr.py
@@ -5,13 +5,21 @@ from prompt_toolkit.formatted_text import FormattedText
 from prompt_toolkit.styles import Style
 import requests
 
+def requestpw(payload):
+    url = 'https://passwordinator.herokuapp.com'
+    response = requests.get(url)
+    return response
+
 class PromptLoop(Cmd):
     prompt = 'pgr>'
     intro = "Password Generator Retriever v1.0.0"
 
     def do_getpw(self, inp):
         printft('TODO: implement getting password....')
-        req = requests.get('https://passwordinator.herokuapp.com')
+        #req = requests.get('https://passwordinator.herokuapp.com')
+        # limit to 5 works
+        #req = requests.get('https://passwordinator.herokuapp.com?len=5')
+        req = requestpw('')
         print("Status code: " + str(req.status_code))
         print("Json returned: " + str(req.json()))
         return False   
